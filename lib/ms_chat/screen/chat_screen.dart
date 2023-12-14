706e6ff4c03bcb1fb744b0674c51328e9256fffc
MukeshSheoran <mukeshsheoran883@gmail.com>
2023-12-14 13:15:20 +0530

    Add remaining UI design for Chat Screen

--- lib/ms_chat/screen/chat_screen.dart
+++ lib/ms_chat/screen/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ms_chat/main.dart';
+import 'package:ms_chat/ms_chat/api/apis.dart';
 import 'package:ms_chat/ms_chat/model/chat_user.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -24,6 +25,47 @@ class _ChatScreenState extends State<ChatScreen> {
         ),
         body: Column(
           children: [
+            Expanded(
+              child: StreamBuilder(
+                 stream: APIs.getAllUsers(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return const Center(
+                        // child: CircularProgressIndicator(),
+                      );
+                    case ConnectionState.active:
+                    case ConnectionState.done:
+                      // final data = snapshot.data?.docs;
+                      // _list = data
+                      //     ?.map((e) => ChatUser.fromJson(e.data()))
+                      //     .toList() ??
+                      //     [];
+                      final _list = [];
+                      if (_list.isNotEmpty) {
+                        return ListView.builder(
+                          itemCount:
+                           _list.length,
+                          padding: EdgeInsets.only(top: mq.height * .01),
+                          physics: const BouncingScrollPhysics(),
+                          itemBuilder: (context, index) {
+
+                            return Text('Message: ${_list[index]} ');
+                          },
+                        );
+                      } else {
+                        return const Center(
+                          child: Text(
+                            'Say Hii ! 👋',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        );
+                      }
+                  }
+                },
+              ),
+            ),
             _chatInput(),
           ],
         ),
